cf599f65d62e5a79e3375ab075ca8bd2c706790d
Timur Minzulin <timurminzulin@MacBook-Air-Timur.local>
2023-07-19 22:52:01 +0300

    Created ShoppingCart component and added changes to ShoppingCartContext

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useContext, useState } from 'react'
-  
+import ShoppingCart from '../components/ShoppingCart'
+
 type ShoppingCartProviderProps = {
   children: ReactNode
 }
@@ -96,6 +97,7 @@ export function ShoppingCartProvider( { children }: ShoppingCartProviderProps ) 
       }}
     >
       {children}
+      <ShoppingCart isOpen={isOpen}/>
     </ShoppingCartContext.Provider>
   )
 }
